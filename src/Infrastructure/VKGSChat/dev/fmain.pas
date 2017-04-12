cbaa8f439de696d4cbbbba4c348328c8067a2cc5
zawuza <andreizawuza@gmail.com>
2017-04-12 17:55:33 +0200

    chat prototype

--- src/Infrastructure/VKGSChat/dev/fmain.pas
+++ src/Infrastructure/VKGSChat/dev/fmain.pas
@@ -15,6 +15,7 @@ type
   TForm1 = class(TForm)
     Chat: TVKGSChat;
     procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { private declarations }
   public
@@ -33,8 +34,21 @@ implementation
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   Chat := TVKGSChat.Create(Self);
-  Chat.Parent:=Self;
-  Chat.Align:=alClient;
+  Chat.Parent := Self;
+  Chat.Align := alClient;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  Chat.Messages.Add(TMessage.Create);
+  Chat.Messages.Add(TMessage.Create);
+  Chat.Messages.Add(TMessage.Create);
+  Chat.Messages[0].Message := 'olfmrieogtmje';
+  Chat.Messages[0].Left := True;
+  Chat.Messages[1].Message := 'Oligofrenot';
+  Chat.Messages[1].Left := False;
+  Chat.Messages[2].Message := 'Pffffffffffffff pffffffffffffffffffffffff vpffffffff';
+  Chat.Messages[2].Left := True;
 end;
 
 end.
--- src/Infrastructure/VKGSChat/dev/vkgschat.pas
+++ src/Infrastructure/VKGSChat/dev/vkgschat.pas
@@ -5,7 +5,7 @@ unit vkgschat;
 interface
 
 uses
-  Classes, SysUtils, Controls, fgl, LCLIntf;
+  Classes, SysUtils, Controls, fgl, LCLIntf, LCLType, Graphics;
 
 type
 
@@ -32,6 +32,9 @@ type
     procedure SetMessages(AValue: TMessegesList);
   protected
     procedure Paint; override;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
   published
     property Messages: TMessegesList read FMessages write SetMessages;
   end;
@@ -42,43 +45,80 @@ implementation
 
 procedure TMessage.SetMessage(AValue: string);
 begin
-  if FMessage=AValue then Exit;
-  FMessage:=AValue;
+  if FMessage = AValue then
+    Exit;
+  FMessage := AValue;
 end;
 
 procedure TMessage.SetLeft(AValue: boolean);
 begin
-  if FLeft=AValue then Exit;
-  FLeft:=AValue;
+  if FLeft = AValue then
+    Exit;
+  FLeft := AValue;
 end;
 
 { TVKGSChat }
 
 procedure TVKGSChat.SetMessages(AValue: TMessegesList);
 begin
-  if FMessages=AValue then Exit;
-  FMessages:=AValue;
+  if FMessages = AValue then
+    Exit;
+  FMessages := AValue;
 end;
 
 procedure TVKGSChat.Paint;
-var LeftBorder, RightBorder, Center, BottomBorder, i, Overlapping: integer;
-    Message: TMessage;
+var
+  LeftBorder, RightBorder, LeftCenter, RightCenter: integer;
+  i: integer;
+  BottomLine, TopLine: integer;
+  DistanceBetween: integer;
+  BottomPadding: integer;
+  Message: TMessage;
+  LRect: TRect;
 begin
   inherited Paint;
 
-  Overlapping := 20;
-  LeftBorder:=30;
-  RightBorder:=Canvas.Width-30;
-  Center := Trunc((RightBorder - LeftBorder)/2);
-  BottomBorder :=0
+  Canvas.Brush.Color := clBlue;
+  Canvas.Pen.Color := clWhite;
+  Canvas.Font.Color:=clWhite;
 
-  for i:=Messages.Count-1 downto 0 do
+  LeftBorder := 30;
+  DistanceBetween := 10;
+  BottomPadding := 10;
+  RightBorder := Canvas.Width - 30;
+  LeftCenter := Trunc((RightBorder - LeftBorder) / 2) - 30;
+  RightCenter := LeftCenter + 60;
+
+  BottomLine := Canvas.Height - BottomPadding;
+
+  for i := Messages.Count - 1 downto 0 do
   begin
-
-
+    Message := Messages[i];
+    LRect := Rect(LeftBorder, BottomLine - Canvas.TextHeight('A'), RightCenter, BottomLine);
+    DrawText(Canvas.Handle, PChar(Message.Message), Message.Message.Length,
+      LRect, DT_CALCRECT or DT_WORDBREAK);
+    Topline := BottomLine - LRect.Height;
+    LRect.Top := Topline;
+    LRect.Bottom := BottomLine;
+    Canvas.Rectangle(LRect);
+    DrawText(Canvas.Handle, PChar(Message.Message), Message.Message.Length,
+      LRect, DT_WORDBREAK);
+    BottomLine := TopLine - DistanceBetween;
+    if BottomLine < 0 then
+      exit;
   end;
 
 end;
 
-end.
+constructor TVKGSChat.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FMessages := TMessegesList.Create(True);
+end;
 
+destructor TVKGSChat.Destroy;
+begin
+  FreeAndNil(FMessages);
+end;
+
+end.
