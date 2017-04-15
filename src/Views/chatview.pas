087dcc5f4ce36939f4aa33effcc2a419c1dd0e76
zawuza <andreizawuza@gmail.com>
2017-04-15 15:40:08 +0200

    VKGSChat integrated

--- src/Views/chatview.pas
+++ src/Views/chatview.pas
@@ -5,7 +5,7 @@ unit ChatView;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, ExtCtrls, ComCtrls, vkgschat;
+  Classes, SysUtils, FileUtil, Forms, Controls, ExtCtrls, ComCtrls, vkgschat, Graphics, chatviewmodel;
 
 type
 
@@ -13,13 +13,16 @@ type
 
   TChatFrameView = class(TFrame)
     ChatView: TVKGSChat;
-    Panel1: TPanel;
+    ChatPanel: TPanel;
     TabControl: TTabControl;
   private
+    FViewModel: IChatViewModel;
+    procedure SetViewModel(AValue: IChatViewModel);
     { private declarations }
   public
     constructor Create(TheOwner: TComponent); override;
     destructor Destroy; override;
+    property ViewModel:IChatViewModel read FViewModel write SetViewModel;
   end;
 
 var
@@ -31,10 +34,37 @@ implementation
 
 { TChatFrameView }
 
+procedure TChatFrameView.SetViewModel(AValue: IChatViewModel);
+begin
+  if FViewModel=AValue then Exit;
+  FViewModel:=AValue;
+end;
+
 constructor TChatFrameView.Create(TheOwner: TComponent);
 begin
   inherited Create(TheOwner);
   ChatView:=TVKGSChat.Create(Self);
+  ChatView.Align:=alClient;
+  ChatView.BoxBorder:=2;
+  ChatView.DistanceBetweenMessages:=10;
+  ChatView.BoxColor:=RGBToColor(93,139,201);
+  ChatView.FrameColor:=clWhite;
+  ChatView.Overlapping:=40;
+  ChatView.PaddingLeft:=30;
+  ChatView.PaddingRight:=30;
+  ChatView.PaddingBottom:=10;
+  ChatView.Font.Color:=clWhite;
+  ChatView.Font.Name:='Segoe UI';
+  ChatView.Font.Size:=12;
+  ChatView.Messages.Add(TMessage.Create);
+  ChatView.Messages[0].Left:=true;
+  ChatView.Messages[0].Message:='Здравствуйте, Иван! '
+  +'Мы прислушались к Вашей просьбе и решили что-то сделать, что понравится Вам! '
+  +'От лица всей фирмы ООО "Пупкин" мы дарим вам танк, который поможет Вам быстрее '
+  +'передвигаться по городу в условиях пробок! За этот невероятный подарок Вам '
+  +'следует поблагодарить Дартаньяна!' + #13#10
+  +'Он ждет Вас :)';
+  ChatView.Parent:=ChatPanel;
 end;
 
 destructor TChatFrameView.Destroy;
